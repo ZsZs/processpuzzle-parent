2f75381fec0eb0b4ee37bd997138789bc7b34d55
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2018-05-24 07:37:55 +0000

    add Chrome installation to the schell script

--- install-java-maven.sh
+++ install-java-maven.sh
@@ -30,4 +30,12 @@ npm install -g npm
 node --version
 npm --version
 
+echo "Install headless Chrome"
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:canonical-chromium-builds/stage
+sudo apt-get update
+sudo apt-get install chromium-browser
+export CHROME_BIN=/usr/bin/chromium-browser
+
 echo "Happy coding!"
